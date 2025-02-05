32d24c468f640a7bcc12aaa255f007410309beef
yuvarajry20 <2k21cse187@kiot.ac.in>
2025-02-05 14:58:15 +0530

    First commit sample java file created

---
+++ DemoProjectGit/src/com/git/Gitdemo.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class Gitdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This code represents Git demo");
+	}
+
+}
