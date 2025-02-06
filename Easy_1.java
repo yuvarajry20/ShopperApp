9f10f9ff5e3d75c8b43c2663b63905d3850c8e9e
yuvarajry20 <146225077+yuvarajry20@users.noreply.github.com>
2025-02-06 16:29:43 +0530

    Update Easy_1.java

--- Easy_1.java
+++ Easy_1.java
@@ -5,9 +5,13 @@ public class Easy_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helo world");
-		String name=args[0];
-		int age=Integer.parseInt(args[1]);
-		System.out.println("Name is "+name+" age is "+age);
+		if (args.length < 2) {
+            System.out.println("Usage: java ParameterInput <name> <age>");
+            return;
+        }
+		        String name = args[0];
+		        int age = Integer.parseInt(args[1]);
+		        System.out.println("Name: " + name + ", Age: " + age);
 	}
 
 }
